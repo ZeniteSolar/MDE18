62df5f168850dad50ce145fc4df2e4c686aa2bc4
João Antônio Cardoso <joao.maker@gmail.com>
2022-03-17 19:08:54 -0300

    Code reformat/cleanup

--- firmware/rabeta_can_pont_erro_teste1106/rabeta_can_pont_erro_teste1106.ino
+++ firmware/rabeta_can_pont_erro_teste1106/rabeta_can_pont_erro_teste1106.ino
@@ -1,50 +1,60 @@
 #include <SPI.h>
 #include <mcp_can.h>
 
+static void enviar();
 
-const int SPI_CS_PIN = 10;
+static const int SPI_CS_PIN = 10; // Pino de saida para CS
+MCP_CAN CAN(SPI_CS_PIN);          // Set CS pin
 
-
-MCP_CAN CAN(SPI_CS_PIN);                                    // Set CS pin
-
-void setup()
+void can_init()
 {
-    Serial.begin(115200);
-
-    while (CAN_OK != CAN.begin(CAN_50KBPS))              // init can bus : baudrate = 50k
+    while (CAN_OK != CAN.begin(CAN_50KBPS))
     {
         Serial.println("CAN BUS Shield init fail");
         Serial.println("Init CAN BUS Shield again");
         delay(100);
     }
     Serial.println("CAN BUS Shield init ok!");
-
-  pinMode(3, OUTPUT); // configura pino como saÃ­da
-  pinMode(5, OUTPUT); // configura pino como saÃ­da
-unsigned char stmp[1] = {123};
 }
 
+void setup()
+{
+    Serial.begin(115200);
+    can_init();
 
+    pinMode(3, OUTPUT); // configura pino como saí­da
+    pinMode(5, OUTPUT); // configura pino como saí­da
+    unsigned char stmp[1] = {123};
+}
+
+static void enviar()
+{
+    unsigned char stmp[1] = {78};
+    Serial.println("Enviando informacao");
+    // send data:  id = 0x00, standard frame, data len = 8, stmp: data buf
+    CAN.sendMsgBuf(0x70, 0, 1, stmp);
+    delay(1); // tempo de envio
+}
 
 void loop()
 {
-    unsigned char len = 0;  //zerando tamanho
-    unsigned char buf[1];   //quantidade de palavras enviada
+    unsigned char len = 0; // zerando tamanho
+    unsigned char buf[1];  // quantidade de palavras recebidas
     unsigned char ang_dir;
-    
-//----------------- Procedimento p/ receber informaçoes da direção -----------------
 
-    if(CAN_MSGAVAIL == CAN.checkReceive())            // check if data coming
+    //----------------- Procedimento p/ receber informaçoes da direção -----------------
+
+    if (CAN_MSGAVAIL == CAN.checkReceive()) // check if data coming
     {
-        CAN.readMsgBuf(&len, buf);    // read data,  len: data length, buf: data buf
+        CAN.readMsgBuf(&len, buf); // read data,  len: data length, buf: data buf
 
         unsigned char canId = CAN.getCanId();
         Serial.print("ID =");
         Serial.println(canId);
 
-        for(int i = 0; i<len; i++)    // print the data
+        for (int i = 0; i < len; i++) // print the data
         {
-            Serial.print("DIRECAO =");            
+            Serial.print("DIRECAO =");
             Serial.println(buf[i]);
             ang_dir = buf[i];
         }
@@ -52,62 +62,53 @@ void loop()
     }
     else
     {
-         Serial.println("Erro comunicacao"); 
-         ang_dir = 128;  
-         delay(1);  //teste se tinha erro 
+        // Serial.println("Erro comunicacao");
+        ang_dir = 128;
+        delay(1); // teste se tinha erro
     }
-//------------------ Calculo/Analise/Ação do angulo da rabeta ----------------------
-//ajustes
-  int inzero = 2; // ajuste do intervalo de zero, estabilidade na direção
-  int potini = 85; // ajuste de potencia inicial do pwm, ajusta a potencia e velocidade do motor
-  int potfin = 90; // ajuste de potencia final do pwm, ajusta a potencia e velocidade do motor
 
-  int sensormotor = analogRead(A0);
-  sensormotor = map(sensormotor,0,1024,0,255);
-  Serial.print("motor = ");
-  Serial.println(sensormotor);
-  delay(5);
- int potmotor = sensormotor - ang_dir;
-  if (potmotor > inzero){
-      Serial.print("positivo = ");
-      Serial.println(potmotor);
-      if (potmotor > potfin){
-          potmotor = potfin; 
-          }
-      potmotor = map(potmotor,0,potfin,potini,255);
-      analogWrite(5, 0);
-      analogWrite(3, potmotor);
-      
-  }
-  else if (potmotor < - inzero){
-      Serial.print("negativo = ");
-      potmotor = potmotor * (-1);
-      Serial.println(potmotor); 
-      if (potmotor > potfin){
-          potmotor = potfin; 
-          }
-      potmotor = map(potmotor,0,potfin,potini,255);
-      analogWrite(5, potmotor);
-      analogWrite(3, 0);
- }
-  else{
-      analogWrite(5, 0);
-      analogWrite(3, 0);    
-    
-  }
+    //------------------ Calculo/Analise/Ação do angulo da rabeta ----------------------
 
+    // ajustes
+    int inzero = 2;  // ajuste do intervalo de zero, estabilidade na direção
+    int potini = 85; // ajuste de potencia inicial do pwm, ajusta a potencia e velocidade do motor
+    int potfin = 90; // ajuste de potencia final do pwm, ajusta a potencia e velocidade do motor
+
+    int sensormotor = analogRead(A0);
+    sensormotor = map(sensormotor, 0, 1024, 0, 255);
+    Serial.print("motor = ");
+    Serial.println(sensormotor);
+    delay(5);
+
+    int potmotor = sensormotor - ang_dir;
+    if (potmotor > inzero)
+    {
+        Serial.print("positivo = ");
+        Serial.println(potmotor);
+        if (potmotor > potfin)
+        {
+            potmotor = potfin;
+        }
+        potmotor = map(potmotor, 0, potfin, potini, 255);
+        analogWrite(5, 0);
+        analogWrite(3, potmotor);
+    }
+    else if (potmotor < -inzero)
+    {
+        Serial.print("negativo = ");
+        potmotor = potmotor * (-1);
+        Serial.println(potmotor);
+        if (potmotor > potfin)
+        {
+            potmotor = potfin;
+        }
+        potmotor = map(potmotor, 0, potfin, potini, 255);
+        analogWrite(5, potmotor);
+        analogWrite(3, 0);
+    }
+    else
+    {
+        analogWrite(5, 0);
+        analogWrite(3, 0);
+    }
 }
-
-//--------------------------- Chamada p/ problema ---------------------------------  
-
-void enviar()
-{
-  unsigned char stmp[1] = {78};
-    Serial.println("Enviando informacao");
-    // send data:  id = 0x00, standard frame, data len = 8, stmp: data buf
-    CAN.sendMsgBuf(0x70,0, 1, stmp);
-    delay(1);                       // tempo de envio
-  
-  }
-
-//END FILE
--- firmware/volante_can_angl/volante_can_angl.ino
+++ firmware/volante_can_angl/volante_can_angl.ino
@@ -1,85 +1,82 @@
 #include <mcp_can.h>
 #include <SPI.h>
 
+static const int SPI_CS_PIN = 10; // Pino de saida para CS
+MCP_CAN CAN(SPI_CS_PIN);          // Set CS pin
 
-const int SPI_CS_PIN = 10; //Pino de saida para CS
-
-MCP_CAN CAN(SPI_CS_PIN);                                    // Set CS pin
-
-void setup()
+void can_init()
 {
-    Serial.begin(115200);
-
-    while (CAN_OK != CAN.begin(CAN_50KBPS))              // init can bus : baudrate = 50k
+    while (CAN_OK != CAN.begin(CAN_50KBPS))
     {
         Serial.println("CAN BUS Shield init fail");
-        Serial.println(" Init CAN BUS Shield again");
+        Serial.println("Init CAN BUS Shield again");
         delay(100);
     }
     Serial.println("CAN BUS Shield init ok!");
 }
 
-unsigned char stmp[1] = {127};
-unsigned long sensorValues = 0;
-int sensorValue = 0;
-int Nmedia = 10;
+void setup()
+{
+    Serial.begin(115200);
+    can_init();
+}
 
-
+static unsigned char stmp[1] = {127};
+static unsigned long sensorValues = 0;
+static int sensorValue = 0;
+static int Nmedia = 10;
 
 void loop()
-{   
-      unsigned char len = 0;
-      unsigned char buf[1]; 
-//---------------------- Procedimento p/ pegar angulo direção -------------------
-  for (int i=0; i <= Nmedia - 1; i++){
-      sensorValue = analogRead(A2);
-      sensorValues = sensorValues + sensorValue;
-     // delay(1);                   // delay in between reads for stability
-   } 
-  sensorValue = sensorValues/Nmedia;
-  Serial.println(sensorValue);
-  sensorValues = 0;
-  sensorValue = map(sensorValue,0,1022,0,255);
-  if (sensorValue > 255){
-    sensorValue = 255;
-  }
-  else if (sensorValue < 0){
-    sensorValue = 0;
-  }
-  stmp[0] = sensorValue;
+{
+    unsigned char len = 0;
+    unsigned char buf[1]; // quantidade de palavras enviada
 
-//---------------------- Procedimento p/ enviar informações ------------------- 
- 
-  Serial.println("Enviando informacao");
-    // send data:  id = 0x00, standard frame, data len = 8, stmp: data buf
-    CAN.sendMsgBuf(0x60,0, 1, stmp);
-    delay(1);                       // tempo de envio
+    //---------------------- Procedimento p/ pegar angulo direção -------------------
 
-    
-//------------------------ Procedimento p/ receber informaçoes -----------------
-
-if(CAN_MSGAVAIL == CAN.checkReceive())            // check if data coming
+    for (int i = 0; i <= Nmedia - 1; i++)
     {
-        CAN.readMsgBuf(&len, buf);    // read data,  len: data length, buf: data buf
+        sensorValue = analogRead(A2);
+        sensorValues = sensorValues + sensorValue;
+        // delay(1);                   // delay in between reads for stability
+    }
+    sensorValue = sensorValues / Nmedia;
+    Serial.println(sensorValue);
+    sensorValues = 0;
+    sensorValue = map(sensorValue, 0, 1022, 0, 255);
+    if (sensorValue > 255)
+    {
+        sensorValue = 255;
+    }
+    else if (sensorValue < 0)
+    {
+        sensorValue = 0;
+    }
+    stmp[0] = sensorValue;
+
+    //---------------------- Procedimento p/ enviar informações -------------------
+
+    Serial.println("Enviando informacao");
+    // send data:  id = 0x00, standard frame, data len = 8, stmp: data buf
+    CAN.sendMsgBuf(0x60, 0, 1, stmp);
+    delay(1); // tempo de envio
+
+    //------------------------ Procedimento p/ receber informaçoes -----------------
+
+    if (CAN_MSGAVAIL == CAN.checkReceive()) // check if data coming
+    {
+        CAN.readMsgBuf(&len, buf); // read data,  len: data length, buf: data buf
 
         unsigned char canId = CAN.getCanId();
 
-        if(canId == 0x70) //Verificando se é o ID da rabeta
+        if (canId == 0x70) // Verificando se é o ID da rabeta
         {
-         Serial.println("Problema");
+            Serial.println("Problema");
 
-          for(int i = 0; i<len; i++)    // print the data
-         {
-             Serial.print(buf[i]);
-
-          }
+            for (int i = 0; i < len; i++) // print the data
+            {
+                Serial.print(buf[i]);
+            }
         }
         Serial.println();
     }
-
-    
 }
-
-/*********************************************************************************************************
-  END FILE
-*********************************************************************************************************/
